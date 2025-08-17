b83dc99482a21b1ca3ce6f7dd07fda3ac209057a
Андрей Ткаченко <andrejtkacenko963@gmail.com>
2025-08-17 08:59:30 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,20 @@
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8JxvjvJzcL0DPVhM1jzEcwUXSS7qPGnE",
-  authDomain: "zenith-flow-5x266.firebaseapp.com",
-  projectId: "zenith-flow-5x266",
-  storageBucket: "zenith-flow-5x266.firebasestorage.app",
-  messagingSenderId: "437158497095",
-  appId: "1:437158497095:web:e2031eece10d7f4e099a62",
+  "projectId": "zenith-flow-5x266",
+  "appId": "1:437158497095:web:e2031eece10d7f4e099a62",
+  "storageBucket": "zenith-flow-5x266.firebasestorage.app",
+  "apiKey": "AIzaSyB8JxvjvJzcL0DPVhM1jzEcwUXSS7qPGnE",
+  "authDomain": "zenith-flow-5x266.firebaseapp.com",
+  "messagingSenderId": "437158497095"
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth };
+export { app, auth, db };
