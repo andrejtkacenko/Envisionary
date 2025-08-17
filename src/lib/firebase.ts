939824279e42e11ed62b789a3be944096974a08b
Андрей Ткаченко <andrejtkacenko963@gmail.com>
2025-08-17 05:13:55 +0000

    все ещё не получается создать аккаунт

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB8JxvjvJzcL0DPVhM1jzEcwUXSS7qPGnE",
+  authDomain: "zenith-flow-5x266.firebaseapp.com",
+  projectId: "zenith-flow-5x266",
+  storageBucket: "zenith-flow-5x266.firebasestorage.app",
+  messagingSenderId: "437158497095",
+  appId: "1:437158497095:web:e2031eece10d7f4e099a62",
 };
 
 // Initialize Firebase
