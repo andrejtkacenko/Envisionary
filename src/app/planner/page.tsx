82cd480a78ae86835325575b29c1d36b8c48d4e2
Андрей Ткаченко <andrejtkacenko963@gmail.com>
2025-08-18 08:58:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/planner/page.tsx
+++ src/app/planner/page.tsx
@@ -34,8 +34,8 @@ const GenerateScheduleInputSchema = z.object({
 });
 
 
-const SortableItem = ({ id, item, isEditing, onUpdate, onRemove }: { id: string, item: ScheduledItem, isEditing: boolean, onUpdate: (time: string, task: string) => void, onRemove: () => void }) => {
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+const SortableItem = ({ item, isEditing, onUpdate, onRemove }: { item: ScheduledItem, isEditing: boolean, onUpdate: (time: string, task: string) => void, onRemove: () => void }) => {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: item.id });
 
     const style = {
         transform: CSS.Transform.toString(transform),
@@ -313,8 +313,7 @@ export default function PlannerPage() {
                                             <div className="space-y-2 pr-4">
                                                 {currentDaySchedule.schedule.map((item, itemIndex) => (
                                                     <SortableItem 
-                                                        key={item.id} 
-                                                        id={item.id}
+                                                        key={item.id}
                                                         item={item} 
                                                         isEditing={isEditing}
                                                         onUpdate={(time, task) => updateItem(itemIndex, time, task)}
@@ -333,3 +332,5 @@ export default function PlannerPage() {
         </div>
     );
 }
+
+    
