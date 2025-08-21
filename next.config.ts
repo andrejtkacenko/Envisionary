a1c0b79145d02757df87944ab4a4c9fbfd54e30a
Андрей Ткаченко <andrejtkacenko963@gmail.com>
2025-08-21 10:57:24 +0000

    ответа от бота также нет. А на https://api.telegram.org/bot8322171796:AA

--- next.config.ts
+++ next.config.ts
@@ -19,22 +19,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-proto',
-            value: 'http',
-          },
-        ],
-        destination: 'https://:host/:path*',
-        permanent: true,
-      },
-    ]
-  },
   // This is the new configuration to allow requests from the Cloud Workstation proxy
   devIndicators: {
     allowedRevalidateOrigins: [
